2fd842f28b3408363d8a4df692af9a6eb5d84403
Alex Ray <jalexray@gmail.com>
2021-08-08 16:38:31 -0700

    Initial commit

---
+++ config.py
@@ -0,0 +1,12 @@
+# Parade Tools - Flask Server (Scribe Automation)
+import os
+from dotenv import load_dotenv
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+class Config(object):
+	SERVER_MODE = os.environ.get('SERVER_MODE') or 'development'
+	
+	# Filepath
+	FILEPATH = basedir
